b2610057ff4a780750dac864d4c3c5b5c3b729c9
Stanley Odhiambomwai <odhiambostanley799@gmail.com>
2024-07-23 11:03:23 +0300

    index.html changes

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+const works = [
+    {
+        id: 1,
+        text: 'walking alongside the street',
+        itscomplited: true
+    },
 
+    {
+        id: 2,
+        text: 'studying online',
+        itscomplited: true
+    },
+
+    {
+       id: 3,
+       text: 'dancing music',
+       itscomplited: false
+    }
+];
+
+const workText = works.map(function(work){
+   return work.text;
+});
+
+console.log(workText)
