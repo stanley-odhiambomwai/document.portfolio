a45ba0fbff374436db60d00fc76571d1ebb3d2ea
Stanley Odhiambomwai <odhiambostanley799@gmail.com>
2024-07-27 21:30:22 +0300

    script.js

---
+++ payment.js
@@ -0,0 +1,10 @@
+//single element
+
+//console.log(document.getElementById('Transaction path form'));
+//console.log(document.querySelector('h2'));
+//console.log(document.querySelector('tr'));
+//console.log(document.querySelector('u'));
+//console.log(document.querySelector('h1'));
+
+// multiple elements
+console.log(document.querySelectorAll('table'))
--- script.js
+++ script.js
@@ -1,25 +1,4 @@
-const works = [
-    {
-        id: 1,
-        text: 'walking alongside the street',
-        itscomplited: true
-    },
-
-    {
-        id: 2,
-        text: 'studying online',
-        itscomplited: true
-    },
-
-    {
-       id: 3,
-       text: 'dancing music',
-       itscomplited: false
-    }
-];
-
-const workText = works.map(function(work){
-   return work.text;
-});
-
-console.log(workText)
+function Name(firstName,lastName){
+  console.log(firstName + lastName)
+}
+Name('John' ,'Wick')
