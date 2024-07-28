8b6cddea652269adfe8a169f70b9ddb52100743d
Stanley Odhiambomwai <odhiambostanley799@gmail.com>
2024-07-28 22:02:57 +0300

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 function Name(firstName,lastName){
   console.log(firstName + lastName)
 }
-Name('John' ,'Wick')
+Name('John','Wick')
+
+function addNumbers(num1,num2){
+  console.log( num1+num2);
+}
+addNumbers(3,4)
+
+function multiplyNumbers(num1,num2){
+  console.log(num1*num2);
+}
+multiplyNumbers(4,3)
+
+
